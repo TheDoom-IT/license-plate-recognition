9f461f7addc070f81bce38899770dc815b6dd5b2
Dawid Badurek <dawidbadurek@wp.pl>
2024-11-21 23:09:32 +0100

    Add OpenCV example

--- license_plate_recognition/main.py
+++ license_plate_recognition/main.py
@@ -1,1 +1,6 @@
+from .recognition.service import RecognitionService
+
 print("Polish license plate recognition system")
+
+recognition_service = RecognitionService()
+recognition_service.recognize()
