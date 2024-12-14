3054ad30521cdacef687d334416ff702e2a9ad49
Dawid Badurek <dawidbadurek@wp.pl>
2024-12-14 20:47:26 +0100

    Create an entrypoint for the application

--- license_plate_recognition/main.py
+++ license_plate_recognition/main.py
@@ -1,1 +1,33 @@
-print("Polish license plate recognition system")
+import sys
+import os.path
+import cv2
+from .recognition.service import RecognitionService
+from .plate_detection.service import PlateDetectionService
+from .segmentation.service import SegmentationService
+
+
+def read_file(filename: str) -> cv2.typing.MatLike:
+    if not os.path.isfile(filename):
+        print(f"File '{filename}' does not exist")
+        sys.exit(1)
+
+    return cv2.imread(filename, cv2.IMREAD_COLOR)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <filename>")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+    img = read_file(filename)
+
+    plate_detection_service = PlateDetectionService()
+    segmentation_service = SegmentationService()
+    recognition_service = RecognitionService()
+
+    plate = plate_detection_service.detect(img)
+    characters = segmentation_service.segment(plate)
+    result = recognition_service.recognize(characters)
+
+    print(f"The image contains the following license plate: {result}")
---
+++ license_plate_recognition/plate_detection/service.py
@@ -0,0 +1,11 @@
+import cv2
+
+
+class PlateDetectionService:
+    def __init__(self):
+        # TODO: load model here
+        pass
+
+    def detect(self, img: cv2.typing.MatLike):
+        # TODO: implement plate detection
+        pass
