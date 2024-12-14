59b1731d3435d49f46a22ae627ef3dda244687dd
Dawid Badurek <dawidbadurek@wp.pl>
2024-12-14 22:24:05 +0100

    Show image instead of printing the result in the console

--- license_plate_recognition/main.py
+++ license_plate_recognition/main.py
@@ -2,7 +2,7 @@ import sys
 import os.path
 import cv2
 from .recognition.service import RecognitionService
-from .plate_detection.service import PlateDetectionService
+from .plate_detection.service import PlateDetectionService, Box
 from .segmentation.service import SegmentationService
 
 
@@ -12,6 +12,13 @@ def read_file(filename: str) -> cv2.typing.MatLike:
         sys.exit(1)
 
     return cv2.imread(filename, cv2.IMREAD_COLOR)
+
+
+def show_result(img: cv2.typing.MatLike, box: Box, plate_number: str):
+    cv2.rectangle(img, (box.x1, box.y1), (box.x2, box.y2), (0, 0, 255), 2)
+    cv2.putText(img, plate_number, (box.x1, box.y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+    cv2.imshow("License Plate Recognition", img)
+    cv2.waitKey(0)
 
 
 if __name__ == '__main__':
@@ -26,8 +33,8 @@ if __name__ == '__main__':
     segmentation_service = SegmentationService()
     recognition_service = RecognitionService()
 
-    plate = plate_detection_service.detect(img)
+    plate, box = plate_detection_service.detect(img)
     characters = segmentation_service.segment(plate)
-    result = recognition_service.recognize(characters)
+    plate_number = recognition_service.recognize(characters)
 
-    print(f"The image contains the following license plate: {result}")
+    show_result(img, box, plate_number)
--- license_plate_recognition/plate_detection/service.py
+++ license_plate_recognition/plate_detection/service.py
@@ -1,4 +1,13 @@
 import cv2
+from dataclasses import dataclass
+
+
+@dataclass
+class Box:
+    x1: int
+    y1: int
+    x2: int
+    y2: int
 
 
 class PlateDetectionService:
@@ -6,6 +15,13 @@ class PlateDetectionService:
         # TODO: load model here
         pass
 
-    def detect(self, img: cv2.typing.MatLike):
+    def detect(self, img: cv2.typing.MatLike) -> tuple[cv2.typing.MatLike, Box]:
         # TODO: implement plate detection
-        pass
+
+        height, width, _ = img.shape
+        return None, Box(
+            int(width / 2),
+            int(height / 2),
+            int(width / 2 + 100),
+            int(height / 2 + 100),
+        )
