11b14774918aef6b20f9c4fa133e6f246dff9c84
Dawid Badurek <dawidbadurek@wp.pl>
2024-12-17 21:49:55 +0100

    Plot results on a single figure

--- license_plate_recognition/main.py
+++ license_plate_recognition/main.py
@@ -1,24 +1,43 @@
 import sys
 import os.path
 import cv2
+import numpy as np
+import matplotlib.pylab as plt
+from matplotlib.gridspec import GridSpec
+from PIL import Image, ImageFile
 from .recognition.service import RecognitionService
 from .plate_detection.service import PlateDetectionService, Box
 from .segmentation.service import SegmentationService
 
 
-def read_file(filename: str) -> cv2.typing.MatLike:
+def read_file(filename: str) -> ImageFile:
     if not os.path.isfile(filename):
         print(f"File '{filename}' does not exist")
         sys.exit(1)
 
-    return cv2.imread(filename, cv2.IMREAD_COLOR)
+    return Image.open(filename)
 
 
-def show_result(img: cv2.typing.MatLike, box: Box, plate_number: str):
-    cv2.rectangle(img, (box.x1, box.y1), (box.x2, box.y2), (0, 0, 255), 2)
-    cv2.putText(img, plate_number, (box.x1, box.y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
-    cv2.imshow("License Plate Recognition", img)
-    cv2.waitKey(0)
+def show_result(img: cv2.typing.MatLike, box: Box, plate: np.ndarray, characters: list[np.ndarray], plate_number: str):
+    gs = GridSpec(3, len(characters))
+
+    fig = plt.figure()
+    fig.suptitle(plate_number, fontsize=16)
+
+    ax1 = fig.add_subplot(gs[0, :])
+    ax1.imshow(img)
+
+    ax1.add_patch(plt.Rectangle((box.x1, box.y1), box.x2 - box.x1, box.y2 - box.y1, fill=False, edgecolor='red', lw=2))
+
+    ax2 = fig.add_subplot(gs[1, :])
+    ax2.imshow(plate)
+
+    for index, character in enumerate(characters):
+        ax = fig.add_subplot(gs[2, index])
+        ax.imshow(character, cmap='gray', interpolation='nearest')
+
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -35,6 +54,9 @@ if __name__ == '__main__':
 
     plate, box = plate_detection_service.detect()
     characters = segmentation_service.segment(plate)
+    if len(characters) == 0:
+        print("No characters found")
+        sys.exit(1)
     plate_number = recognition_service.recognize(characters)
 
-    show_result(img, box, plate_number)
+    show_result(img, box, plate, characters, plate_number)
