5565b412645258333624d819851d1b600db8856d
Dawid Badurek <56804600+TheDoom-IT@users.noreply.github.com>
2024-12-21 22:22:49 +0100

    Implement RecognitionService
    * Implement RecognitionService

--- license_plate_recognition/recognition/service.py
+++ license_plate_recognition/recognition/service.py
@@ -1,8 +1,59 @@
-class RecognitionService:
-    def __init__(self):
-        # TODO: load model here
-        pass
+import keras
+from PIL import Image, ImageOps
+import numpy as np
+import os
 
-    def recognize(self, characters: list) -> str:
-        # TODO: implement recognition here
-        return "ABC1234"
+
+class RecognitionService:
+    EXPECTED_WIDTH = 27
+    EXPECTED_HEIGHT = 40
+    CLASSES = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    model: keras.api.models.Sequential
+
+    def __init__(self):
+        # keras.config.disable_interactive_logging()
+        self.model = self.__load_model()
+
+    @staticmethod
+    def __load_model():
+        script_dir = os.path.dirname(__file__)
+        model_name = "recognition_model.keras"
+        model_path = os.path.join(script_dir, "..", "..", ".weights", model_name)
+
+        return keras.models.load_model(model_path)
+
+    def recognize(self, characters: list[np.ndarray]) -> str:
+        result = ""
+
+        input = np.array([self.__adjust_image_size(character) for character in characters])
+        predictions = self.model.predict(input)
+        for prediction in predictions:
+            predicted_class = np.argmax(prediction)
+            result += self.CLASSES[predicted_class]
+
+        return result
+
+    def __adjust_image_size(self, image: np.ndarray) -> np.ndarray:
+        """
+        Adjust the image size to the expected size of the neural network.
+        Add padding if necessary.
+        """
+        img = Image.fromarray(image)
+        # resize keeping the aspect ratio
+        img = ImageOps.contain(img, (self.EXPECTED_WIDTH, self.EXPECTED_HEIGHT), Image.Resampling.NEAREST)
+
+        # add padding if image is smaller than the expected size
+        result = np.array(img)
+        if result.shape[0] < self.EXPECTED_HEIGHT:
+            new_array = np.zeros((self.EXPECTED_HEIGHT, result.shape[1]), dtype=np.uint8)
+            start = (self.EXPECTED_HEIGHT - result.shape[0]) // 2
+            new_array[start:(result.shape[0] + start), :] = result
+            result = new_array
+
+        if result.shape[1] < self.EXPECTED_WIDTH:
+            new_array = np.zeros((result.shape[0], self.EXPECTED_WIDTH), dtype=np.uint8)
+            start = (self.EXPECTED_WIDTH - result.shape[1]) // 2
+            new_array[:, start:(result.shape[1] + start)] = result
+            result = new_array
+
+        return result
